cf42d6a6ce42ee836f120aa271577bf8a74130dc
Niteesh Putla <90021910+NiteeshPutla@users.noreply.github.com>
2023-02-17 00:03:39 +0530

    Update CrowdFund.sol

--- contracts/CrowdFund.sol
+++ contracts/CrowdFund.sol
@@ -1,25 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+//ERC20 functions used in the contract 
 interface IERC20 {
     function transfer(address, uint) external returns (bool);
     function transferFrom(address, address, uint) external returns (bool);
     function balanceOf(address) external view returns (uint);
 }
 
+//defining state variables for the contract
 contract CrowdFunding {
     address public manager;
     uint256 public goal;
     uint256 public totalFunding;
+    
+    //mapping to monitor the funds each address that contributed
     mapping(address => uint256) public contributions;
+
+    //instance of token for contributions
     IERC20 public immutable token;
+
+    //bool to check whether the goal is reached
     bool public isGoalReached;
 
+    //event for emitting when a fund transfer occurs
     event FundTransfer(
         address from,
         address to,
         uint256 value
     );
 
+    //initializing the state varibles 
     constructor (
         address _token,
         address _manager,
@@ -43,6 +54,7 @@ contract CrowdFunding {
         contributions[msg.sender] += transferredAmount;
         totalFunding += transferredAmount;
 
+        //emitting the fund transfer when the contribution is greater than zero.
         emit FundTransfer(msg.sender, manager, transferredAmount);
 
         if (totalFunding >= goal) {
@@ -59,6 +71,20 @@ contract CrowdFunding {
 
         emit FundTransfer(address(this), manager, totalFunding);
     }
+
+    function refund() public {
+        require(isGoalReached == false, "Goal has been reached, refunds are not possible");
+        require(contributions[msg.sender] > 0, "Sender has not contributed any funds");
+
+        uint256 amountToRefund = contributions[msg.sender];
+        contributions[msg.sender] = 0;
+        totalFunding -= amountToRefund;
+
+        token.transfer(msg.sender, amountToRefund);
+
+        emit FundTransfer(address(this), msg.sender, amountToRefund);
+    }
+}
 
     function refund() public {
         require(isGoalReached == false, "Goal has been reached, refunds are not possible");
